f103768cc441323678f0248312afc1dc57360044
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-26 15:50:37 +0500

    app colors =

---
+++ lib/res/app_colors/app_colors.dart
@@ -0,0 +1,13 @@
+
+
+
+import 'package:instagram_clone/res/files_paths/files_paths.dart';
+
+class MyColors {
+
+  static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.white;
+  static const Color greyColor = Colors.grey;
+  static const Color darkPinkColor = Color(0xffc54685);
+
+}
