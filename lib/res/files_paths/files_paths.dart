1cd3bd64f5059db32f0c21fc65afa78198fbee89
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-29 16:29:52 +0500

    updated

--- lib/res/files_paths/files_paths.dart
+++ lib/res/files_paths/files_paths.dart
@@ -1,5 +1,15 @@
 
 
 export 'package:flutter/material.dart';
+
 export 'package:instagram_clone/view/splash_view/splash_view.dart';
+export 'package:instagram_clone/view/auth_view/sign_in_view/sign_in_view.dart';
+export 'package:instagram_clone/view_model/splash_view_model/splash_view_model.dart';
+
+//////////////// Components Paths ///////////////////
+
+export 'package:instagram_clone/res/components/my_text.dart';
+export 'package:instagram_clone/res/app_colors/app_colors.dart';
+
+
 
