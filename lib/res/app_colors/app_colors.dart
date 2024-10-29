f886c6691bb3814fbb643569220997e2d0223b06
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-29 16:29:38 +0500

    new color is added

--- lib/res/app_colors/app_colors.dart
+++ lib/res/app_colors/app_colors.dart
@@ -5,8 +5,10 @@ import 'package:instagram_clone/res/files_paths/files_paths.dart';
 
 class MyColors {
 
+  static const Color backgroundColor = Color(0xFFedf5ff);
   static const Color whiteColor = Colors.white;
-  static const Color blackColor = Colors.white;
+  static const Color blackColor = Colors.black;
+  static const Color blueColor = Color(0xFF0d4cae);
   static const Color greyColor = Colors.grey;
   static const Color darkPinkColor = Color(0xffc54685);
 
