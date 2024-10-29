41ab53d673abca179add28ba19d34e0d1d65d145
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-29 16:30:21 +0500

    ui is completed

--- lib/view/auth_view/sign_in_view/sign_in_view.dart
+++ lib/view/auth_view/sign_in_view/sign_in_view.dart
@@ -1,13 +1,120 @@
+import 'package:instagram_clone/res/components/my_text_button.dart';
+import 'package:instagram_clone/res/components/my_text_form_field.dart';
 import 'package:instagram_clone/res/files_paths/files_paths.dart';
-
+import 'package:instagram_clone/view/auth_view/sign_up_view/sign_up_view.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Column(),
+      backgroundColor: MyColors.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: MyColors.backgroundColor,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 13),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              /// ///// Language /////
+              MyText(title: "English (US)"),
+              SizedBox(height: height * 0.1,),
+
+              /// Logo
+              Image.asset(
+                "assets/images/logo.png",
+                width: 70,
+                height: 60,
+              ),
+
+              Column(
+                children: [
+                  SizedBox(
+                    height: height * 0.1,
+                  ),
+
+                  /// ///// Form /////
+                  Form(
+                      child: Column(
+                        children: [
+                          /// User name OR Email
+                          MyTextFormFeild(
+                            hintText: "Username , email",
+                            borderSide: BorderSide(
+                                color: MyColors.greyColor.withOpacity(0.9)),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+
+                          /// Password
+                          MyTextFormFeild(
+                            hintText: "Password",
+                            obscureText: true,
+                            borderSide: BorderSide(
+                                color: MyColors.greyColor.withOpacity(0.9)),
+                          ),
+                          SizedBox(
+                            height: height * 0.02,
+                          ),
+
+                          /// Login Button
+                          MyTextButton(
+                            title: "Log in",
+                            backgroundColor: MyColors.blueColor,
+                            height: height * 0.06,
+                            borderRadius: 26,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            textColor: MyColors.whiteColor,
+                          ),
+                          SizedBox(height: 10,),
+
+                          /// Forget password
+                          MyTextButton(
+                            title: "Forgot Password?",
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                            textColor: MyColors.blackColor.withOpacity(0.7),
+                          )
+                        ],
+                      )),
+                ],
+              ),
+              SizedBox(height: height * 0.2,),
+
+              /// Create New Account
+              Column(
+                children: [
+                  MyTextButton(
+                    title: "Create new account",
+                    fontSize: 18,
+                    textColor: MyColors.blueColor,
+                    border: Border.all(color: MyColors.blueColor),
+                    borderRadius: 26,
+                    height: height * 0.06,
+                    onTap: () => Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUpView())),
+                  ),
+                  MyText(
+                    title: "Meta",
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: MyColors.blackColor.withOpacity(0.7),
+                  ),
+
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
