f0e2e8195df8d07038054c5f7bfb0670eee319a5
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-30 21:14:02 +0500

    bottom navigator view

---
+++ lib/view/bottom_nav_view/bottom_nav_view.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class BottomNavView extends StatelessWidget {
+  const BottomNavView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(),);
+  }
+}
