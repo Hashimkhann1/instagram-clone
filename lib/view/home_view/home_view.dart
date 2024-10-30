19fc41c88b6c3bce113c53dd4caaffb3b306c20a
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-30 21:14:29 +0500

    home view

---
+++ lib/view/home_view/home_view.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomeVierw extends StatelessWidget {
+  const HomeVierw({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
