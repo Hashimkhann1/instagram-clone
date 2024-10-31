6dfffaccd8eb7d9c024da8bdf8d81481e2c4e210
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-31 16:26:30 +0500

    bottom nav bar

--- lib/view/bottom_nav_view/bottom_nav_view.dart
+++ lib/view/bottom_nav_view/bottom_nav_view.dart
@@ -1,12 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_clone/res/files_paths/files_paths.dart';
 
 
-import 'package:flutter/material.dart';
 
-class BottomNavView extends StatelessWidget {
+
+class BottomNavView extends StatefulWidget {
   const BottomNavView({super.key});
 
   @override
+  State<BottomNavView> createState() => _BottomNavViewState();
+}
+
+class _BottomNavViewState extends State<BottomNavView> {
+
+  int selectedView = 0;
+
+  List views = [
+
+    HomeVierw(),
+    SearchView(),
+    AddPostView(),
+    ReelsView(),
+    ProfileView()
+
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(),);
+    return Scaffold(
+      body: views[selectedView],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedView,
+        elevation: 0,
+        selectedItemColor: MyColors.blackColor,
+        onTap: (index) {
+          setState(() {
+            selectedView = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home,size: 32,),label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.search,size: 32,),label: ''),
+          BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.squarePlus),label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.ondemand_video_rounded,size: 32,),label: ''),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.person_alt_circle_fill,size: 32,),label: ''),
+        ],
+      ),
+    );
   }
 }
