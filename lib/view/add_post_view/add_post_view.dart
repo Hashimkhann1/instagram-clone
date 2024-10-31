28340e44becb8ae537600cd9b8b42987e0d4172a
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-31 16:27:25 +0500

    add post view

---
+++ lib/view/add_post_view/add_post_view.dart
@@ -0,0 +1,11 @@
+import 'package:instagram_clone/res/files_paths/files_paths.dart';
+
+
+class AddPostView extends StatelessWidget {
+  const AddPostView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
