7a2d415d51a4c2e8a6311679030308a6ec098689
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-31 16:26:43 +0500

    home view

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,14 +1,40 @@
-
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_clone/res/components/my_text.dart';
+import 'package:instagram_clone/view/home_view/stories_view/stories_view.dart';
 
 class HomeVierw extends StatelessWidget {
   const HomeVierw({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Column(),
+      appBar: AppBar(
+        title: MyText(title: "Instagram",),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.heart)),
+          IconButton(onPressed: (){}, icon: FaIcon(FontAwesomeIcons.facebookMessenger)),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.black,
+            height: height * 0.14,
+            child: StoriesView(),
+          ),
+          Expanded(
+            child: Column(
+              children: [],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
