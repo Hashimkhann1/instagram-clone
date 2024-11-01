bc2f3e99dc51584bcf1ccb88d861daf5c8563948
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-01 21:14:07 +0500

    home view ui is almost completed

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,39 +1,43 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:instagram_clone/res/components/my_text.dart';
-import 'package:instagram_clone/view/home_view/stories_view/stories_view.dart';
+import 'package:instagram_clone/res/files_paths/files_paths.dart';
+import 'package:instagram_clone/view/home_view/post_view/post_view.dart';
 
-class HomeVierw extends StatelessWidget {
-  const HomeVierw({super.key});
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       appBar: AppBar(
-        title: MyText(title: "Instagram",),
+        title: MyText(title: "Instagram"),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.heart)),
-          IconButton(onPressed: (){}, icon: FaIcon(FontAwesomeIcons.facebookMessenger)),
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.heart)),
+          IconButton(onPressed: () {}, icon: FaIcon(FontAwesomeIcons.facebookMessenger)),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.black,
-            height: height * 0.14,
-            child: StoriesView(),
-          ),
-          Expanded(
-            child: Column(
-              children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// Stories section
+            Container(
+              height: height * 0.14, // Adjust height as needed for stories
+              child: StoriesView(),
             ),
-          )
-        ],
+
+            /// post data
+            ListView.builder(
+              itemCount: 10,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context , index) {
+                  return PostView();
+                })
+
+          ],
+        ),
       ),
     );
   }
