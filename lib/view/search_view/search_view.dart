c5b41a2e32479b6a5a71763e2f30a00defbe1188
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-07 22:21:50 +0500

    search view

---
+++ lib/view/search_view/search_view.dart
@@ -0,0 +1,11 @@
+import 'package:instagram_clone/res/files_paths/files_paths.dart';
+
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
