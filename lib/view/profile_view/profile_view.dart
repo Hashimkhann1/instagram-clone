bf6709537bf26081dc5d0a483ccb444d3c582873
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-07 22:21:23 +0500

    profile view

---
+++ lib/view/profile_view/profile_view.dart
@@ -0,0 +1,10 @@
+import 'package:instagram_clone/res/files_paths/files_paths.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
