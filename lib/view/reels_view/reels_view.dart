5fd730e2e3a849e380572c087ad2d84b96f238ce
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-07 22:21:35 +0500

    reels view

---
+++ lib/view/reels_view/reels_view.dart
@@ -0,0 +1,11 @@
+import 'package:instagram_clone/res/files_paths/files_paths.dart';
+
+
+class ReelsView extends StatelessWidget {
+  const ReelsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
